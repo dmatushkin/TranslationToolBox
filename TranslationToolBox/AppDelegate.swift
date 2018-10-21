5eda720cc1e09e59a568ee171ac0680bbec954e9
dmatushkin <dmatushkin@gmail.com>
2018-10-21 11:17:45 +0400

    Initial Commit

---
+++ TranslationToolBox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TranslationToolBox
+//
+//  Created by Dmitry Matyushkin on 21/10/2018.
+//  Copyright © 2018 Dmitry Matyushkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
